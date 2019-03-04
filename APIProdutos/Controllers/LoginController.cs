0fe86759056bd17799947fa91d09592f0ef2d2d0
Renato Groffe <renato.groffe@yahoo.com.br>
2019-03-04 16:43:27 -0300

    ASP.NET Core 2.2 + JWT (JSON Web Token) + Identity Core + Entity Framework Core InMemory + Refresh Tokens
    ASP.NET Core 2.2 + JWT (JSON Web Token) + Identity Core + Entity Framework Core InMemory + Refresh Tokens

---
+++ APIProdutos/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using APIProdutos.Security;
+
+namespace APIProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public object Post(
+            [FromBody]AccessCredentials credenciais,
+            [FromServices]AccessManager accessManager)
+        {
+            if (accessManager.ValidateCredentials(credenciais))
+            {
+                return accessManager.GenerateToken(credenciais);
+            }
+            else
+            {
+                return new
+                {
+                    Authenticated = false,
+                    Message = "Falha ao autenticar"
+                };
+            }
+        }
+    }
+}
